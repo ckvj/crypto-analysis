dc91f18a0c52969b3ad9eecda7ee4d9dd20b2907
ckvj <carlkvogel@gmail.com>
2023-03-07 23:27:23 -0500

    small refactor

--- config.py
+++ config.py
@@ -3,26 +3,35 @@ from decimal import Decimal
 
 
 def process_config():
+    """Pulls info from config.ini which must be stored in same folder as main.py
+    
+    Returns:
+        config_dict: dictionary of config.ini
+        _column_dtypes: dictionary of {column names : dtype } to apply to data frame
+        _converter: dictionary of {column names : dtype } to apply via converter (eg needed for Decimal type)
+    
+    """
+
     config = configparser.ConfigParser()
     config.read('config.ini')
     
     config_dict = {s:dict(config.items(s)) for s in config.sections()}
     
-    # Set dir to None if not provided
+    # Set dir of CSV to None if not provided
     if config_dict['file_info'].get('dir', None) == None:
         config_dict['file_info']['dir'] = None
         
-    # Create Buy Type List
+    # Create List of buy types
     config_dict['buy_types_list'] = [x for x in list(config_dict['buy_txn_types'].values())]
 
-    # Create Sell Type List
+    # Create List of sell types
     config_dict['sell_types_list'] = [x for x in list(config_dict['sell_txn_types'].values())]
 
-    # Identify Columns to convert from string
-    _column_dtypes = {config_dict['csv_columns']['price_paid'] : float, config_dict['csv_columns']['quote_asset_amount'] : float}
+    # Identify Columns to convert from string to other dtype
+    _column_dtypes = {config_dict['csv_columns']['quote_asset_amount'] : float}
     _converter = {config_dict['csv_columns']['base_asset_amount'] : Decimal}
     
-    # Data Validations
+    # Accounting Type Validations
     _supported_accounting_types = ['FIFO', 'LIFO', 'HIFO']
     if config_dict['accounting_type']['accounting_type'] not in _supported_accounting_types:
         raise ValueError('Unsupported Analysis Type')
